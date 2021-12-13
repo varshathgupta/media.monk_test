b74c77603f70abcf72514e0a40e9963f7bb6a77c
varshathgupta <varshathgupta@gmail.com>
2021-12-13 23:40:59 +0530

    Done Atlast

--- script.js
+++ script.js
@@ -1,23 +1,104 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// wrapping the code with onload to execute JS immediately
+window.onload = function() {
+	//variables for slide animation time
+	var slideDelay = 1.2; //the slides flow in every 1.5 seconds
+	var slideDuration = 0.2;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+	var slides = document.querySelectorAll(".slide");
+	var prevButton = document.querySelector("#prevButton");
+	var nextButton = document.querySelector("#nextButton");
 
+	var numSlides = slides.length;
 
+	//infinite slide rotation
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("container");
+	for (var i = 0; i < numSlides; i++) {
+		TweenLite.set(slides[i], {
+			xPercent: i * 100
+		});
+	}
 
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  
-  slides[slideIndex-1].style.display = "block";
-  
+	// auto animation (the timer can be added to auto animate after a certain idle-time)
+
+	var wrap = wrapPartial(-100, (numSlides - 1) * 100);
+	var timer = TweenLite.delayedCall(3, autoPlay);
+
+	var animation = TweenMax.to(slides, 1, {
+		xPercent: "+=" + (numSlides * 100),
+		ease: Linear.easeNone,
+		paused: true,
+		repeat: -1,
+		modifiers: {
+			xPercent: wrap
+		}
+	});
+
+	var proxy = document.createElement("div");
+	TweenLite.set(proxy, { x: "+=0" });
+	var transform = proxy._gsTransform;
+	var slideAnimation = TweenLite.to({}, 0.1, {});
+	var slideWidth = 0;
+	var wrapWidth = 0;
+	resize();
+
+	window.addEventListener("resize", resize);
+
+	// navigation with buttons
+	prevButton.addEventListener("click", function() {
+		animateSlides(1);
+	});
+
+	nextButton.addEventListener("click", function() {
+		animateSlides(-1);
+	});
+
+	function animateSlides(direction) {
+		timer.restart(true);
+		slideAnimation.kill();
+
+		var x = snapX(transform.x + direction * slideWidth);
+
+		slideAnimation = TweenLite.to(proxy, slideDuration, {
+			x: x,
+			onUpdate: updateProgress
+		});
+	}
+
+	// auto-play function for auto animation
+
+	function autoPlay() {
+		animateSlides(-1);
+	}
+
+	function updateProgress() {
+		animation.progress(transform.x / wrapWidth);
+	}
+
+	function snapX(x) {
+		return Math.round(x / slideWidth) * slideWidth;
+	}
+
+	//calculating the necessary width for slide animation
+	function resize() {
+		var norm = (transform.x / wrapWidth) || 0;
+
+		slideWidth = slides[0].offsetWidth;
+		wrapWidth = slideWidth * numSlides;
+
+		TweenLite.set(proxy, {
+			x: norm * wrapWidth
+		});
+
+		animateSlides(0);
+		slideAnimation.progress(1);
+	}
+
+	//returns the difference between the passed function's max and min value
+	function wrapPartial(min, max) {
+		var diff = max - min;
+		return function(value) {
+			var v = value - min;
+			return ((diff + v % diff) % diff) + min;
+		}
+	}
 }
